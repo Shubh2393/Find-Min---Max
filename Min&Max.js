ab804159007001445d3996c787aeab27e754f5bc
Shubh2393 <shubhsharma.sharma7@gmail.com>
2024-11-18 15:56:27 +0530

    Min & Max

--- Min&Max.js
+++ Min&Max.js
@@ -1,7 +1,7 @@
 const rangevalue = document.querySelector(".slider-container, .price-slider");
 const rangeInputvalue = document.querySelectorAll(".range-input input");
 
-let priceGap = 500;
+let priceGap = 500; 
 
 const priceInputvalue = document.querySelectorAll(".price-input input");
 for(let i=0; i< priceInputvalue.length; i++) {
